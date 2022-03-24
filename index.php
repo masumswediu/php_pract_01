1f56e1612fc28d49de022e26b00f43f1b9d8cb03
masum <masfikur35-3118@diu.edu.bd>
2022-03-25 02:10:52 +0600

    practis 3/25/22

---
+++ index.php
@@ -0,0 +1,72 @@
+<!--  php used in html
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>
+         <?php echo "hello world"; ?> 
+    </h1>
+</body>
+
+</html> -->
+
+<!-- 
+<?php
+
+
+$name = "Masum";
+$age = 23;
+$address = "Mohammadpur , Dhaka";
+$ammount = 20.5;
+
+// echo $name . ' ' . $age . ' ' . $address . ' par tmie hour rate '. $ammount ; concatination
+
+
+// echo "my name is {$name} ,i live in {$address} . and my age is {$age}. my pertime work hour is {$ammount} ";
+// printf( "my name is {$name} ,i live in {$address} . and my age is {$age}. my pertime work hour is {$ammount} ");
+ $myOutput = sprintf("My name is %s <br> my address is %s ", $name, $address );
+ echo $myOutput;
+
+
+
+?> -->
+
+<!-- 
+<?php
+
+$name = "Masum"; //string [dobble cotetion er moddhe thakle string hoy]
+
+$age = 23; // purno  number thekle integer hoy
+
+$ammount = 25.365; //dosomik sonkha hoile float/dobble
+
+$switch = false ; // true,false agulo boolean  
+
+// echo gettype($name); //variable ber korar neom
+
+
+
+// $student = array('masum', 'hasi'); //array 
+// echo gettype($student);
+
+
+$variable = null;
+
+echo gettype($variable);//null variable .eikhaen kono variable nai 
+
+
+
+
+
+
+
+
+
+?> -->
